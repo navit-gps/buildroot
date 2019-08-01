1e4ffcacf0c46ed0ceaeab6ff900bc923191ccdf
Pierre Grandin <grandinp@gmail.com>
2019-08-01 10:49:10 -0600

    Backported some changes from pgrandin/navit-buildroot

--- package/navit/navit.mk
+++ package/navit/navit.mk
@@ -5,11 +5,34 @@
 ################################################################################
 
 NAVIT_VERSION = trunk
-NAVIT_SITE = git://github.com/navit-gps/navit 
+NAVIT_SITE = git://github.com/navit-gps/navit
 NAVIT_LICENSE = GPL-2
 NAVIT_INSTALL_TARGET = YES
-NAVIT_DEPENDENCIES = host-pkgconf libgtk2
+NAVIT_DEPENDENCIES = host-pkgconf
+NAVIT_MAKE = $(MAKE1)
+NAVIT_CONF_OPTS = -DSVG2PNG=FALSE -DLIBDIR=/usr/lib -DSAMPLE_MAP=0
 
-NAVIT_CONF_OPTS = -DSVG2PNG=FALSE -DLIBDIR=/usr/lib
+ifeq ($(BR2_PACKAGE_NAVIT_GTK2),y)
+NAVIT_DEPENDENCIES += libgtk2
+NAVIT_CONF_OPTS += -Dgraphics/gtk_drawing_area=1
+else
+NAVIT_CONF_OPTS += -Dgraphics/gtk_drawing_area=0
+endif
+
+ifeq ($(BR2_PACKAGE_NAVIT_QT5),y)
+NAVIT_DEPENDENCIES += qt5svg qt5declarative
+NAVIT_CONF_OPTS += -Dgraphics/qt5=1
+else
+NAVIT_CONF_OPTS += -DDISABLE_QT=1 -Dgraphics/qt5=0 -Dvehicle/qt5=0
+endif
+
+ifeq ($(BR2_PACKAGE_NAVIT_GPSD_VEHICLE),y)
+NAVIT_DEPENDENCIES += gpsd
+NAVIT_CONF_OPTS += -Dvehicle/gpsd=1
+endif
+
+ifeq ($(BR2_PACKAGE_NAVIT_J1850_PLUGIN),y)
+NAVIT_CONF_OPTS += -Dplugin/j1850=1
+endif
 
 $(eval $(cmake-package))
